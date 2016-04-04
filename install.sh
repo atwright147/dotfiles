2c53b2baa85bc289f96e1355e214aba770a503de
Andy Wright <atwright147@gmail.com>
2016-04-04 23:51:33 +0100

    fix installer path

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 # http://www.thegeekstuff.com/2010/10/linux-ln-command-examples/
 # ln -s /full/path/of/original/file /full/path/of/soft/link/file
-ln -sf /Users/hxj907/dotfiles/.zshrc /Users/hxj907/.zshrc
+ln -sf $HOME/dotfiles/.zshrc $HOME/.zshrc
