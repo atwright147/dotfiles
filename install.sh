7073ee14c05196e77eedfd89ef91261db075812d
Andy Wright <atwright147@gmail.com>
2016-04-04 16:42:42 +0100

    fix install script

--- install.sh
+++ install.sh
@@ -1,1 +1,3 @@
-ln .zshrc ~/.zshrc
+# http://www.thegeekstuff.com/2010/10/linux-ln-command-examples/
+# ln -s /full/path/of/original/file /full/path/of/soft/link/file
+ln -sf /Users/hxj907/dotfiles/.zshrc /Users/hxj907/.zshrc
