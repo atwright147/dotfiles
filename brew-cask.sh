f55a75314b58271bc0b7aa0c28753ef25845a03f
Andy Wright <atwright147@gmail.com>
2019-02-09 22:45:46 +0000

    add brew scripts

---
+++ brew-cask.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env sh
+
+# quicklook
+brew cask install qlcolorcode
+brew cask install qlstephen
+brew cask install quicklook-json
+
+# development
+brew cask install docker
+brew cask install docker-toolbox
+brew cask install sourcetree
+brew cask install insomnia
+brew cask install iterm2
+brew cask install virtualbox
+brew cask install visual-studio-code
+
+# utilities
+brew cask install fluor
+brew cask install notable
+brew cask install scroll-reverser
+brew cask install dropbox
+
+# music
+brew cask install eqmac
+brew cask install marshallofsound-google-play-music-player
+
+# browsers
+brew cask install firefox
+brew cask install google-chrome
+brew cask install opera
---
+++ brew-packages.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+
+# vcs
+brew install git
+brew install hub
+
+# testing
+brew install httpie
+brew install nikto
+
+# development
+brew install sqlite
+brew install python3
+brew install python@2
+
+# auth
+brew install lastpass-cli
+brew install ssh-copy-id
+
+# utils
+brew install tldr
+brew install unison
+brew install wget
+
+# shells
+brew install zsh
