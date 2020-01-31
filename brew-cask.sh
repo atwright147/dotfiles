197f04cb73f8817f6bea26d7e6c6ff48b0d336f9
Andy Wright <atwright147@gmail.com>
2020-01-31 12:29:45 +0000

    Update brew-packages.sh, brew-cask.sh files

--- brew-cask.sh
+++ brew-cask.sh
@@ -2,29 +2,43 @@
 
 # quicklook
 brew cask install qlcolorcode
+brew cask install qlmarkdown
 brew cask install qlstephen
 brew cask install quicklook-json
 
-# development
+# virtualisation
 brew cask install docker
 brew cask install docker-toolbox
-brew cask install sourcetree
+brew cask install vagrant
+brew cask install vagrant-manager
+brew cask install virtualbox
+brew cask install virtualbox-extension-pack
+
+# development
+brew cask install beyond-compare
+brew cask install colour-contrast-analyser
 brew cask install insomnia
 brew cask install iterm2
-brew cask install virtualbox
+brew cask install sourcetree
 brew cask install visual-studio-code
 
 # utilities
-brew cask install fluor
-brew cask install notable
-brew cask install scroll-reverser
+brew cask install daisydisk
 brew cask install dropbox
+brew cask install fluor
+brew cask install forklift
+brew cask install keepingyouawake  # like amphetamine
+brew cask install notable
+brew cask install numi
+brew cask install rectangle  # like spectacle
+brew cask install scroll-reverser
+brew cask install the-unarchiver
 
 # music
-brew cask install eqmac
+brew cask install boom-3d
 brew cask install marshallofsound-google-play-music-player
 
 # browsers
 brew cask install firefox
 brew cask install google-chrome
-brew cask install opera
+brew cask install microsoft-edge
--- brew-packages.sh
+++ brew-packages.sh
@@ -9,15 +9,17 @@ brew install httpie
 brew install nikto
 
 # development
-brew install sqlite
+# brew install python@2
 brew install python3
-brew install python@2
+brew install sqlite
+brew install yarn
 
 # auth
 brew install lastpass-cli
 brew install ssh-copy-id
 
 # utils
+brew install ack
 brew install tldr
 brew install unison
 brew install wget
