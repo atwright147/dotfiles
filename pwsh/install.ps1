790ba779dfc3f5d01cd35ebf95871fa699f2e0c1
Andy Wright <andy.wright@onyxinsight.com>
2023-09-29 17:32:31 +0100

    use $profile to get profile location

--- pwsh/install.ps1
+++ pwsh/install.ps1
@@ -1,3 +1,3 @@
 # https://conradtheprogrammer.medium.com/save-your-powershell-profile-in-your-dotfiles-repo-8ec723532934
 
-New-Item -Path ~\Documents\PowerShell\Microsoft.PowerShell_profile.ps1 -ItemType SymbolicLink -Value (Get-Item ".\Microsoft.PowerShell_profile.ps1").FullName -Force
+New-Item -Path $profile -ItemType SymbolicLink -Value (Get-Item ".\Microsoft.PowerShell_profile.ps1").FullName -Force
