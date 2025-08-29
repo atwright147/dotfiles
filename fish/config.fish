12fff232ef175d809c6a59b31de03ea198369b97
Andy Wright <atwright147@gmail.com>
2025-08-29 21:06:29 +0100

    add feedback for missing binaries

--- fish/config.fish
+++ fish/config.fish
@@ -3,14 +3,18 @@ if status is-interactive
 end
 
 # Added by LM Studio CLI (lms)
-set -gx PATH $PATH /Users/andy/.lmstudio/bin
+set -gx PATH $PATH "$HOME/.lmstudio/bin"
 set -gx PATH $PATH "$HOME/.local/bin" $PATH
 set -gx PATH $PATH "$HOME/.cargo/bin" $PATH
 
 if command -v oh-my-posh > /dev/null
   oh-my-posh init fish --config ~/dotfiles/prompt.omp.json | source
+else
+  echo "oh-my-posh not found, please install it from https://ohmyposh.dev/docs/installation/linux"
 end
 
 if command -v zoxide > /dev/null
   zoxide init fish | source
+else
+  echo "zoxide not found, please install it from https://github.com/ajeetdsouza/zoxide"
 end
