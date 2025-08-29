0a1ec0afe43e9b59d32740c42ff1c8c6cb976b77
Andy Wright <atwright147@gmail.com>
2025-08-29 21:06:42 +0100

    add better description

--- fish/functions/start_docker.fish
+++ fish/functions/start_docker.fish
@@ -1,12 +1,12 @@
-function start_docker -d "Start Docker daemon and wait for it to be ready"
+function start_docker -d "Start Docker daemon and wait for it to be ready. Usage: start_docker && docker start <container_name> && ..."
   sudo service docker start
-  
+
   # Wait for Docker daemon to start
   while not docker info >/dev/null 2>&1
     echo "Waiting for Docker daemon to start..."
     sleep 1
   end
-  
+
   echo "Docker daemon started."
   return 0
 end
