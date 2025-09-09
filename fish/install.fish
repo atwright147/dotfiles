7fa558a092e4ff38a268afe1546e1111f18bed2c
Andy Wright <atwright147@gmail.com>
2025-09-09 11:46:53 +0100

    Add installation command for Oh My Posh

--- fish/install.fish
+++ fish/install.fish
@@ -5,3 +5,6 @@ ln -s (pwd) ~/.config/fish
 
 # install Cargo (and Rust obvs)
 curl https://sh.rustup.rs -sSf | sh
+
+# install Oh My Posh (same command can be used to update too)
+curl -s https://ohmyposh.dev/install.sh | bash -s
