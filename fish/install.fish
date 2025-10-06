f9c5667c656eec27f590e10476ad9c1eee9b26d2
Andy Wright <atwright147@gmail.com>
2025-10-06 20:58:31 +0100

    install gitignore

--- fish/install.fish
+++ fish/install.fish
@@ -2,6 +2,8 @@
 
 rm -rf ~/.config/fish
 ln -s (pwd) ~/.config/fish
+rm -f ~/.gitconfig
+ln -s (realpath (pwd)/../gitconfig) ~/.gitconfig
 
 # install Oh My Posh (same command can be used to update too)
 curl -s https://ohmyposh.dev/install.sh | bash -s &
