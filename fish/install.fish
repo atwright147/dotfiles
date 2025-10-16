ed8eeb461f63649424f005ccc0e82d1e119c9e40
Andy Wright <atwright147@gmail.com>
2025-10-16 21:10:27 +0100

    fix issues

--- fish/install.fish
+++ fish/install.fish
@@ -1,13 +1,17 @@
 #!/usr/bin/env fish
 
+# Get the absolute path to the dotfiles directory
+set dotfiles_dir (dirname (realpath (status --current-filename)))
+set fish_dir (realpath (status --current-filename | xargs dirname))
+
 rm -rf ~/.config/fish
-ln -s (pwd) ~/.config/fish
+ln -s "$fish_dir" ~/.config/fish
 rm -f ~/.gitconfig
-ln -s (realpath (pwd)/../gitconfig) ~/.gitconfig
+ln -s "$dotfiles_dir/gitconfig" ~/.gitconfig
 
 # install Oh My Posh (same command can be used to update too)
 # curl -s https://ohmyposh.dev/install.sh | bash -s &
 # wait
 # echo "oh-my-posh installed successfully!"
 # echo "Close this terminal, open a new one, then run the following command to install a nerd font:"
-set_color yellow && echo "oh-my-posh font install firacode"
+# set_color yellow && echo "oh-my-posh font install firacode"
