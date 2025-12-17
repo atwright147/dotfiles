510bed6f4810fb5f5ce87e46c4c19f9de60cfd4f
Andy Wright <andy.wright@onyxinsight.com>
2025-12-17 11:34:06 +0000

    remove winget installs from setup file

--- pwsh/Setup.ps1
+++ pwsh/Setup.ps1
@@ -1,9 +1,2 @@
 Install-Module -Name Terminal-Icons
 Install-Module -Name Npm-Completion
-
-winget install ajeetdsouza.zoxide
-winget install junegunn.fzf
-winget install sharkdp.fd
-winget install BurntSushi.ripgrep.MSVC
-winget install ast-grep.ast-grep
-winget install eza-community.eza
