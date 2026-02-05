28f225100381caff6c38e7c374372f402fd67436
Andy Wright <atwright147@gmail.com>
2026-02-05 17:29:51 +0000

    lm studio

--- fish/config.fish
+++ fish/config.fish
@@ -7,6 +7,8 @@ end
 set -gx PATH $PATH "$HOME/.lmstudio/bin"
 set -gx PATH $PATH "$HOME/.local/bin" $PATH
 set -gx PATH $PATH "$HOME/.cargo/bin" $PATH
+set -gx GOPATH "$HOME/go"
+set -gx PATH $PATH "$GOPATH/bin"
 
 if command -v oh-my-posh > /dev/null
   oh-my-posh init fish --config ~/dotfiles/prompt.omp.json | source
@@ -19,3 +21,8 @@ if command -v zoxide > /dev/null
 else
   echo "zoxide not found, please install it from https://github.com/ajeetdsouza/zoxide"
 end
+
+# Added by LM Studio CLI (lms)
+set -gx PATH $PATH /Users/andy/.lmstudio/bin
+# End of LM Studio CLI section
+
